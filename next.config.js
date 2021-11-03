bf0d7bcf1da94c924b069180045384917a3c61a7
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-11-03 10:43:23 -0500

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
+
+const {
+  API_URL: _API_URL,
+  NODE_ENV,
+  VERCEL_GIT_COMMIT_SHA: COMMIT_SHA,
+  VERCEL_GIT_COMMIT_REF,
+  ASSET_PREFIX_CREHANA,
+} = process.env;
+
+const API_URL = _API_URL || '';
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: '/docs',
+  trailingSlash: true,
+  assetPrefix: '',
+  // images: {
+  //   deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  // },
+  env: {
+    API_URL,
+    NEXT_PUBLIC_COMMIT_SHA: COMMIT_SHA,
+  },
 }
